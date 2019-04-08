f40858ede9879d5ae04979baae09130a24ed0d40
ete <stefanvallet99@gmail.com>
2019-04-08 19:28:20 -0400

    Initial commit

---
+++ grafico.java
@@ -0,0 +1,53 @@
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.data.xy.IntervalXYDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+public class grafico extends ApplicationFrame {
+	public grafico(double[] vector) {
+		super("Frecuencias");
+		JPanel chartPanel = crearPanel(vector);
+		chartPanel.setPreferredSize(new java.awt.Dimension(500, 475));
+		setContentPane(chartPanel);
+		this.pack();
+		RefineryUtilities.centerFrameOnScreen(this);
+		this.setVisible(true);
+	}
+	
+	public static JPanel crearPanel(double[] vector) {
+		JFreeChart chart = crearChart(crearDataset(vector));
+		return new ChartPanel(chart);
+	}
+	
+	private static IntervalXYDataset crearDataset(double[] vector) {
+		HistogramDataset dataset = new HistogramDataset();
+		dataset.addSeries("Frecuencias de claves", vector, 8);
+		return dataset;
+	}
+	
+	private static JFreeChart crearChart(IntervalXYDataset dataset) {
+		JFreeChart chart = ChartFactory.createHistogram(
+		"Histograma",
+		"frecuencias",
+		"valor de la clave",
+		dataset,
+		PlotOrientation.VERTICAL,
+		true,
+		true,
+		false
+		);
+		XYPlot plot = (XYPlot) chart.getPlot();
+		XYBarRenderer renderer = (XYBarRenderer) plot.getRenderer();
+		renderer.setDrawBarOutline(false);
+		return chart;
+	}
+
+
+}
---
+++ m.java
@@ -0,0 +1,15 @@
+
+public class m {
+	public static void main(String[] args)  {
+		double vector[] = {63, 89, 36, 49, 56, 64, 59, 35, 78,
+		43, 53, 70, 57, 62, 43, 68, 62, 26,
+		64, 72, 52, 51, 62, 60, 71, 61, 55,
+		59, 60, 67, 57, 67, 61, 67, 51, 81,
+		53, 64, 76, 44, 73, 56, 62, 63, 60};
+		
+		new grafico(vector);
+//		histo.pack();
+//		RefineryUtilities.centerFrameOnScreen(histo);
+//		histo.setVisible(true);
+	}
+}
